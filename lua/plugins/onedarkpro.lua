5180c4afbc7d93f8b529486adda5abe3ce50da64
jcpmbhc <jpineda@banhcafe.hn>
2024-07-18 19:15:24 -0600

    firs commit

---
+++ lua/plugins/onedarkpro.lua
@@ -0,0 +1,12 @@
+return {
+	"olimorris/onedarkpro.nvim",
+	priority = 1000, -- Ensure it loads first
+	config = function()
+		require("onedarkpro").setup({
+			options = {
+				cursorline = true,
+			},
+		})
+		--  vim.cmd("colorscheme onedark_dark")
+	end,
+}
