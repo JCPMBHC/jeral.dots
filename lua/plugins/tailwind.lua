1bdf04b6ca43e03946e9b2727d1d4bf732ae0f88
jcpmbhc <jpineda@banhcafe.hn>
2024-07-24 12:06:59 -0600

    fix: add keymaps for tailwind class

--- lua/plugins/tailwind.lua
+++ lua/plugins/tailwind.lua
@@ -34,11 +34,15 @@ return {
 			-- set keymaps
       local keymap = vim.keymap -- for conciseness
 
-      keymap.set("n", "<leader>tb", "<cmd>TailwindConcealToggle<cr>", { desc = "Toggles conceal class" })
+      keymap.set("n", "<leader>tb", "<cmd>TailwindConcealToggle<cr>", { desc = "Toggles conceal class." })
 
-      keymap.set("n", "<leader>tg", "<cmd>TailwindColorToggle<cr>", { desc = "Toggles color hints" })
+      keymap.set("n", "<leader>tg", "<cmd>TailwindColorToggle<cr>", { desc = "Toggles color hints." })
 
-      keymap.set("n", "<leader>ts", "<cmd>TailwindSort<cr>", { desc = "Sorts all classes in the current buffer" })
+      keymap.set("n", "<leader>ts", "<cmd>TailwindSort<cr>", { desc = "Sorts all classes in the current buffer." })
+
+      keymap.set("n", "<leader>nc", "<cmd>TailwindNextClass<cr>", { desc = "Moves the cursor to the nearest next class node." })
+
+      keymap.set("n", "<leader>pc", "<cmd>TailwindPrevClass<cr>", { desc = "Moves the cursor to the nearest previous class node." })
 
 		end,
 	}
