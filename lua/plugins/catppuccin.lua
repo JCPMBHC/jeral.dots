80b4aba6bd346b0ade64340bd15b9d015341ab0f
jcpmbhc <jpineda@banhcafe.hn>
2024-08-02 11:01:12 -0600

    fix: update glugins and catppuccin

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -5,7 +5,7 @@ return {
   priority = 1000,
   config = function()
     require("catppuccin").setup({
-			transparent_background = true,
+      transparent_background = true,
       color_overrides = {
         all = {},
         latte = {},
@@ -15,6 +15,17 @@ return {
           --base = "#15141f",
         },
       },
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = false,
+        mini = {
+          enabled = true,
+          indentscope_color = "",
+        },
+      }
     })
 
     vim.cmd.colorscheme("catppuccin-mocha")
